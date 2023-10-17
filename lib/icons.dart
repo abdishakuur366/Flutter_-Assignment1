47da5b80ed864f2bab361f38d59dde3ddf1e5c0f
Abdishakuur Hassan <abdishakuurh366@gmail.com>
2023-10-17 01:23:28 -0700

    first commit

---
+++ lib/icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App1 extends StatelessWidget {
+  const App1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
