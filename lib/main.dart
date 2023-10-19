c60057e05e91971668c6ed30da4de8e0755d6a3a
Abdishakuur Hassan <abdishakuurh366@gmail.com>
2023-10-18 20:59:09 -0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:first_flutter_project/icons.dart';
 import 'package:first_flutter_project/myApp.dart';
 import 'package:first_flutter_project/pizza.dart';
+import 'package:first_flutter_project/social.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: App1(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: myApp(),
+    ),
+  );
 }
 
 
--- lib/social.dart
+++ lib/social.dart
@@ -15,92 +15,96 @@ class social extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Card(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      maxRadius: 20,
-                      backgroundImage: AssetImage('images/s.jpg'),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    //Column kaan waxaa kujiro Magaca iyo Waqtiga
-                    Column(
-                      children: [
-                        Text(
-                          "Abdishakuur Hassan",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Container(
-                            margin: EdgeInsets.only(right: 50),
-                            child: Text("5 mins ago"))
-                      ],
-                    ),
-                    //Container kaan waxaa kujiro Icon ka geeska yaalo oo kor
-                    Container(
-                        margin: EdgeInsets.only(
-                          left: 180,
-                          top: 10,
-                        ),
-                        width: 30,
-                        child:
-                            //Icon(Icons.three_g_mobiledata)
-                            Image(
-                          image: AssetImage('images/dots.png'),
-                        ))
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                //Rowga Sawirka, Magaca, Time ka iyo ICON ka wuu dhamaaday
-                Image(
-                  image: AssetImage('images/poster.png'),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 40, bottom: 5),
-                      width: 30,
-                      child: Image(
-                        image: AssetImage('images/like.png'),
+            child: Container(
+              margin: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.all(1),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        maxRadius: 20,
+                        backgroundImage: AssetImage('images/s.jpg'),
                       ),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Text(
-                      "Like",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Icon(
-                      Icons.comment,
-                      size: 30,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text("Comment"),
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Icon(Icons.share),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text("Share")
-                  ],
-                )
-              ],
+                      SizedBox(
+                        width: 3,
+                      ),
+                      //Column kaan waxaa kujiro Magaca iyo Waqtiga
+                      Column(
+                        children: [
+                          Text(
+                            "Abdishakuur Hassan",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Container(
+                              margin: EdgeInsets.only(right: 50),
+                              child: Text("5 mins ago"))
+                        ],
+                      ),
+                      //Container kaan waxaa kujiro Icon ka geeska yaalo oo kor
+                      Container(
+                          margin: EdgeInsets.only(
+                            left: 180,
+                            top: 10,
+                          ),
+                          width: 25,
+                          child:
+                              //Icon(Icons.three_g_mobiledata)
+                              Image(
+                            image: AssetImage('images/dots.png'),
+                          ))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  //Rowga Sawirka, Magaca, Time ka iyo ICON ka wuu dhamaaday
+                  Image(
+                    image: AssetImage('images/poster.png'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 40, bottom: 5),
+                        width: 28,
+                        child: Image(
+                          image: AssetImage('images/like.png'),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "Like",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Icon(
+                        Icons.comment,
+                        size: 30,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text("Comment"),
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Icon(Icons.share),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text("Share")
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
